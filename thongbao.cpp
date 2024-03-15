560fe705669cc8a55d304112056163bdde26441b
MinhQuan23012 <quantran.2301@gmail.com>
2024-03-15 16:03:11 +0700

    them thong bao

--- thongbao.cpp
+++ thongbao.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main()
 {
 	cout << "56_Trần Minh Quân" << endl;
-	cout << "Bài thực hành buổi 1 công nghệ phần mềm" << endl;
+	cout << "Bài thực hành buổi 1 kiểm thử phần mềm" << endl;
 	return 0;
 }
