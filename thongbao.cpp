cf448ada74b3319a70232f7e04ab93c1931463ed
MinhQuan23012 <quantran.2301@gmail.com>
2024-03-15 16:12:34 +0700

    themfile

--- thongbao.cpp
+++ thongbao.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	cout << "56_Trần Minh Quân" << endl;
 	cout << "Bài thực hành buổi 1 công nghệ phần mềm" << endl;
-	cout << " Trần Minh Quân";
+	cout << " Trần Minh Quân" << endl;
+	cout << "Bài thực hành buổi 2" << endl;
 	return 0;
 }
